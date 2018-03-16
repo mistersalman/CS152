d2329c1edac3b8b64e68e3ef5a14c1851dea58d7
ryangreay <rgray003@ucr.edu>
2018-03-16 09:34:17 -0700

    Update semanticValues.h

--- phase3/semanticValues.h
+++ phase3/semanticValues.h
@@ -1,1 +1,18 @@
+#ifndef __semanticValues_h__
+#define __semanticValues_h__
 
+ #include <vector>
+ #include <string>
+ #include <iostream>
+ using namespace std; //don't wanna add std:: to everything
+
+struct semanticValues {
+  	int* place;
+  	string* type;
+	  string* val;
+	  string* index;
+	  vector<string>* valSet;
+	  vector<varParams>* varSet;
+	  vector<exprParams>* exprSet;
+} 
+#endif
